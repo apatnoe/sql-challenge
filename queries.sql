7410aa22cb65e225380dbc4069b8c50b107a3f5a
Allison Patnoe <allison.patnoe@gmail.com>
2021-03-06 13:37:25 -0600

    final queries

--- queries.sql
+++ queries.sql
@@ -1,31 +1,67 @@
--- First select title and id for movie EARLY HOME
-SELECT title, film_id
-FROM film
-WHERE title = 'EARLY HOME';
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
--- Using the film_id located in the previous query find it in the inventory table
-SELECT *
-FROM inventory
-WHERE film_id = 268;
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM "Employees" e
+JOIN "Salaries" s
+ON (e.emp_no = s.emp_no)
 
--- Use Join to find the inventory, film and store id
-SELECT i.inventory_id, i.film_id, i.store_id
-FROM inventory i
-JOIN film f
-ON (i.film_id = f.film_id)
-WHERE f.title = 'EARLY HOME';
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
 
--- Use Subquery to get the film_id from the query finding EARLY HOME
-SELECT *
-FROM inventory
-WHERE film_id IN
-(
-  SELECT film_id
-  FROM film
-  WHERE title = 'EARLY HOME'
-);
+SELECT first_name, last_name, hire_date 
+FROM "Employees"
+Where hire_date between '1/1/1986' AND '12/31/1986'
+ORDER BY hire_date
 
--- Check to more sure the subquery returned correct value
-SELECT *
-FROM inventory
-WHERE film_id IN (268);
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+SELECT d.dept_no, d.dept_name, mgr.emp_no, e.last_name, e.first_name
+FROM "Departments" d
+JOIN "Dep_Managers" mgr
+ON (d.dept_no = mgr.dept_no)
+JOIN "Employees" e
+ON (mgr.emp_no = e.emp_no)
+	
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Dep_Employees" de
+JOIN "Employees" e
+ON (de.emp_no = e.emp_no)
+JOIN "Departments" d
+ON (de.dept_no = d.dept_no)
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name, last_name, sex from "Employees"
+	WHERE first_name = 'Hercules' AND last_name LIKE 'B%'; 
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Departments" d
+JOIN "Dep_Employees" de
+ON (d.dept_no = de.dept_no)
+JOIN "Employees" e
+ON (de.emp_no = e.emp_no)
+WHERE d.dept_name = 'Sales'
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "Departments" d
+JOIN "Dep_Employees" de
+ON (d.dept_no = de.dept_no)
+JOIN "Employees" e
+ON (de.emp_no = e.emp_no)
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+ORDER BY dept_name
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name)
+FROM "Employees"
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC
+
+
+
